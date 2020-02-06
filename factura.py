a04fb03b046a8f80a760a07e01e820f502cc57df
pabloares <36982835+pabloares@users.noreply.github.com>
2020-02-06 13:58:49 +0100

    Delete factura.py

--- factura.py
+++
@@ -1,6 +0,0 @@
-factura = float(input("Introduce la factura hasta dos decimales: "))
-iva = factura * 0.21
-propina = factura * 0.1
-print("El IVA es", iva)
-print("La propina es", propina)
-print("La factura total es", factura + iva + propina)
