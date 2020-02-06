20aa30f259468203b024e19f610abd8d92889fa3
pabloares <36982835+pabloares@users.noreply.github.com>
2020-02-06 13:57:50 +0100

    Delete area-habitacion.py

--- area-habitacion.py
+++
@@ -1,3 +0,0 @@
-ancho = int(input("Introduce el ancho de la habitacion: "))
-largo = int(input("Introduce el largo de la habitacion: "))
-print("El area de la habitacion es", ancho * largo, "metros cuadrados")
