8977ef21ce13a4dc13c58ee6a2cbf8d099380595
pabloares <36982835+pabloares@users.noreply.github.com>
2020-02-06 13:58:17 +0100

    Delete triangle.py

--- triangle.py
+++
@@ -1,7 +0,0 @@
-s1 = float(input("Introduce el primer lado: "))
-s2 = float(input("Introduce el segundo lado: "))
-s3 = float(input("Introduce el tercero lado: "))
-from math import sqrt
-s = (s1 + s2 + s3) / 2
-product = s * (s - s1) * (s - s2) * (s - s3)
-print("El area es", sqrt(product))
